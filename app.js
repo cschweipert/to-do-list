ffb1a7dec4a1c6812c4a1979f6f3158cd57d8f52
Christina Schweipert <c.schweipert@gmail.com>
2019-08-16 18:48:23 -0600

    get date with toLocaleDateString and format with options object

--- app.js
+++ app.js
@@ -10,37 +10,20 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res) {
 
   var today = new Date(); //plain JS
-  var currentDay = today.getDay(); //plain JS
-  var day = ""; //create an empty variable
 
-  switch (currentDay) {
-    case 0:
-    day = "Sunday";
-    break;
-    case 1:
-    day = "Monday";
-    break;
-    case 2:
-    day = "Tuesday";
-    break;
-    case 3:
-    day = "Wednesday";
-    break;
-    case 4:
-    day = "Thursday";
-    break;
-    case 5:
-    day = "Friday";
-    break;
-    case 6:
-    day = "Saturday";
-    break;
-    default:
-    console.log("Error: current day is equal to: " + currentDay);
-  }
+  //options object to display the day in a specific format
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-//do all the computing and logic first and
-//then only pass over the result of that logic
+  //get local date and format it with the option object
+  var day = today.toLocaleDateString("en-US", options);
+
+
+  //do all the computing and logic first and
+  //then only pass over the result of that logic
   res.render("list", {
     kindOfDay: day
   });
